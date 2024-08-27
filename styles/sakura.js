569ffe14374d13938a2ab962687cf8fb1036401b
qiuchengju-github <2804033984@qq.com>
2024-08-27 09:35:52 +0800

    Site updated: 2024-08-27 09:35:52

--- styles/sakura.js
+++ styles/sakura.js
@@ -116,7 +116,8 @@ function startSakura() {
     "position: fixed;left: 0;top: 0;pointer-events: none;"
   );
   canvas.setAttribute("id", "canvas_sakura");
-  document.getElementsByTagName("body")[0].appendChild(canvas);
+  document.querySelector('body').appendChild(canvas);
+  // document.getElementsByTagName("body")[0].appendChild(canvas);
   cxt = canvas.getContext("2d");
   var sakuraList = new SakuraList();
   for (var i = 0; i < 50; i++) {
